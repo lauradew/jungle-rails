c3637827a85b30e33ae78c001797cfdbc8689fd5
Laura De Witt <lauramdewitt@gmail.com>
2018-03-04 05:55:19 +0000

    implemented average ratings

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,10 @@ class ProductsController < ApplicationController
     @review = Review.new
     @product_review = Review.where(product_id: params[:id])
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if @product_review.average(:rating)
+     @average_rating = @product_review.average(:rating).round(2)
+     @average_rating = "#{@average_rating}".sub(".0","")
+    end
   end
 
 end
